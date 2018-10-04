610ebfb8acf35f9b6ca7835e3789881caad95f2b
jspenc72 <jspenc72@gmail.com>
2018-10-03 21:50:46 -0600

    Initial Project WatchOS / iOS App Commit

---
+++ OpenBCI/OpenBCI/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OpenBCI
+//
+//  Created by Jesse Spencer on 10/3/18.
+//  Copyright © 2018 cindt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("viewDidLoad()")
+    }
+
+
+}
+
